9c1fa5548530237cba6cf51b0c5617b25bc9378a
Francis <francisglacken@gmail.com>
2019-02-25 03:23:12 -0500

    Added code for a RoomDatabase

--- app/src/main/java/com/mani/rc/MainViewModel.java
+++ app/src/main/java/com/mani/rc/MainViewModel.java
@@ -1,4 +1,48 @@
 package com.mani.rc;
 
-public class MainViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import com.mani.rc.Database.Category;
+import com.mani.rc.Database.CategoryRepository;
+
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+    private CategoryRepository repository;
+
+    // Category variables
+    private LiveData<List<Category>> allCategories;
+    private LiveData<List<Category>> favorites;
+    private LiveData<Category> category;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        repository = new CategoryRepository(application);
+        allCategories = repository.getAllCategories();
+    }
+
+    public LiveData<List<Category>> getAllCategories() {
+        return allCategories;
+    }
+
+    public LiveData<Category> getCategoryByTitle(String title) {
+        category = repository.getCategoryByTitle(title);
+        return category;
+    }
+
+    public void insertCategory(Category category) {
+        repository.insertCategory(category);
+    }
+
+    public void updateCategory(Category category) {
+        repository.updateCategory(category);
+    }
+
+    public LiveData<List<Category>> getFavorites() {
+        favorites = repository.getFavorites();
+        return favorites;
+    }
 }
